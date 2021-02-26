c3e15c1a2f762667bfa4a1ddb24072d95cb772be
hancel-avila <61561709+hancel-avila@users.noreply.github.com>
2021-02-26 13:58:07 -0600

    Add files via upload

---
+++ resources/js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+// const btnLogin = document.querySelector('.login__btn');
+
+// const account1 = {
+//   username: 'js',
+//   pin: 1111
+// };
+
+// const accounts = [account1, account2, account3];
+
+////////////////////
+// Event handlers - Login
+// let currentAccount;
+
+// btnLogin.addEventListener('click', function (e) {
+//   // Prevent form from submitting
+//   e.preventDefault();
+
+//   currentAccount = account.find(
+//     acc => acc.username === inputLoginUsername.value
+//   );
+//   console.log(currentAccount);
+//   }
